94695d9e2b43cb6559b98d3825500700d6dde2e7
msecret <msegreto@miceover.com>
2015-07-14 21:06:10 -0700

    Use SAT for collisions
    This uses the SAT library todo collisions rather then own. This
    allows more complex collision functionality such as calculating
    circles and polygons. It also gets rid of the update method as
    the check collision code will be checked from a game manager
    that has access to all the objects. This was done so each
    entity collision object wouldn't have to keep a reference to
    all other entities.

--- src/components/collision.js
+++ src/components/collision.js
@@ -1,4 +1,6 @@
 
+import SAT from 'sat';
+
 /**
  * CollisionEventSchema
  * {
@@ -12,62 +14,28 @@
 export var collision = {
 
   init(entity, props) {
-    this._entities = this._entities || [];
-    this._entities.push(entity);
   },
 
-  update(entity) {
-    var collision = false;
+  checkCollision(entityA, entityB) {
+    var response = new SAT.Response(),
+        collided = false,
+        method;
 
-    for (let entityCompare of this._entities) {
-      if (entityCompare !== entity) {
-        if (entity.isRounded) {
-          collision = this.roundedIntersects(entity, entityCompare);
-        } else {
-          collision = this.rectangularIntersects(entity, entityCompare);
-          if (collision) {
-            entity.emit('collision', {
-              intersection: collision,
-              area: collision.w * collision.y,
-              entityA: entity,
-              entityB: entityCompare,
-              type: 'rectangular'
-            });
-          }
-        }
-      }
-    }
-  },
-
-  /**
-   * Checks when two rectangles intersect and either returns false for when
-   * theres no intersection or the x and y coordinates if they do.
-   * @param {Entity} entityA One of the entitys to check.
-   * @param {Entity} entityB One of the entitys to check.
-   * @return {Boolean|Object} Either false or the x/y point of intersection.
-   */
-  rectangularIntersects(entityA, entityB) {
-    var xOverlap,
-        yOverlap,
-        xa1 = entityA.x,
-        ya1 = entityA.y,
-        xa2 = entityA.x + entityA.w,
-        ya2 = entityA.y + entityA.h,
-        xb1 = entityB.x,
-        yb1 = entityB.y,
-        xb2 = entityB.x + entityB.w,
-        yb2 = entityB.y + entityB.h,
-        collision = false;
-
-    let ix = Math.max(xa1, xb1);
-    let iw = Math.max(0, Math.min(xa2, xb2) - ix);
-    let iy = Math.max(ya1, yb1);
-    let ih = Math.max(0, Math.min(ya2, yb2) - iy);
-
-    if (iw !== 0 || ih !== 0) {
-      collision = {x: ix, y: iy, w: iw, h: ih};
+    if (entityA.isRounded && entityB.isRounded) {
+      method = SAT.testCircleCircle;
+    } else if (entityA.isRounded) {
+      method = SAT.testCirclePolygon;
+    } else if (entityB.isRounded) {
+      method = SAT.testPolygonCircle;
+    } else {
+      method = SAT.testPolygonPolygon;
     }
 
-    return collision;
+    collision = method(entityA, entityB, response);
+    if (collision) {
+      return response;
+    }
+
+    return false;
   }
 };
--- test/unit/components/collision_test.js
+++ test/unit/components/collision_test.js
@@ -21,131 +21,15 @@ var test = redtape({
 
 /**
  * =============================================================================
- * update()
+ * checkCollision()
  * =============================================================================
  */
-test('update() will not call a collision event if there are no other entities',
-    t => {
-  var TestE = makeEntityProto(),
-      spy = sandbox.spy(),
-      localCollision = Object.create(collision),
-      testE = TestE.make();
-
-  testE.init({x: 10, y: 10, w: 10, h: 10});
-  testE.on('collision', spy);
-  localCollision.init(testE);
-  localCollision.update(testE);
-
-  t.ok(!spy.called, 'the collision listener was not called');
+test('checkCollision() will return false if no collision', t => {
 
   t.end();
 });
 
-test('update() will call a collision event if theres two intersecting entities',
-    t => {
-  var TestE = makeEntityProto(),
-      spy = sandbox.spy(),
-      localCollision = Object.create(collision),
-      test1 = TestE.make(),
-      test2 = TestE.make();
-
-  test1.init({x: 10, y: 10, w: 20, h: 20});
-  test2.init({x: 15, y: 15, w: 10, h: 20});
-  test1.on('collision', spy);
-  localCollision.init(test1);
-  localCollision.init(test2);
-  localCollision.update(test1);
-
-  let collided = collision.rectangularIntersects(test1, test2);
-  t.ok(collided, 'theres an intersection between two entities');
-
-  t.ok(spy.calledOnce, 'the collision listener was called once');
-  t.equal(spy.args[0][0].area, 150, 'passes the area to the listener');
-  t.equal(spy.args[0][0].entityA, test1, 'passes the first entity to listener');
-  t.equal(spy.args[0][0].entityB, test2, 'passes the 2nd entity to listener');
-  t.equal(spy.args[0][0].type, 'rectangular', 'type is rectangular');
+test('checkCollision() will return an SAT response if there\' a collision', t => {
 
   t.end();
 });
-
-test('update() will call many collision events',
-    t => {
-  var TestE = makeEntityProto(),
-      spy = sandbox.spy(),
-      localCollision = Object.create(collision),
-      test1 = TestE.make(),
-      test2 = TestE.make(),
-      test3 = TestE.make();
-
-  test1.init({x: 10, y: 10, w: 20, h: 20});
-  test2.init({x: 15, y: 15, w: 10, h: 20});
-  test3.init({x: 5, y: 5, w: 10, h: 10});
-  test1.on('collision', spy);
-  localCollision.init(test1);
-  localCollision.init(test2);
-  localCollision.init(test3);
-  localCollision.update(test1);
-
-  let collided = collision.rectangularIntersects(test1, test2);
-  t.ok(collided, 'theres an intersection between two entities');
-  collided = collision.rectangularIntersects(test1, test3);
-  t.ok(collided, 'theres an intersection between other two entities');
-
-  t.equal(spy.callCount, 2, 'collision listener was called thrice');
-  t.equal(spy.args[0][0].entityA, test1, 'passes the first entity to listener');
-  t.equal(spy.args[0][0].entityB, test2, 'passes the 2nd entity to listener');
-  t.equal(spy.args[1][0].entityA, test1, 'passes the first entity to listener');
-  t.equal(spy.args[1][0].entityB, test3, 'passes the 3rd entity to listener');
-
-  t.end();
-});
-
-/**
- * =============================================================================
- * rectangularIntersects()
- * =============================================================================
- */
-test('rectangularIntersects() returns false if no intersection', t => {
-  var entityA,
-      entityB,
-      actual;
-
-  entityA = {x: 10, y: 10, w: 20, h: 20};
-  entityB = {x: 40, y: 40, w: 10, h: 10};
-
-  actual = collision.rectangularIntersects(entityA, entityB);
-
-  t.ok(!actual, 'returns false');
-
-  t.end();
-});
-
-test('rectangularIntersects() returns coordinates if intersection', t => {
-  var entityA,
-      entityB,
-      actual;
-
-  entityA = {x: 10, y: 10, w: 20, h: 20};
-  entityB = {x: 20, y: 20, w: 10, h: 10};
-
-  actual = collision.rectangularIntersects(entityA, entityB);
-
-  t.ok(actual, 'returns truthy');
-  t.equal(actual.w, 10, 'returns width of interesction');
-  t.equal(actual.h, 10, 'returns height of intersection');
-  t.equal(actual.x, 20, 'returns x of interesction');
-  t.equal(actual.y, 20, 'returns y of intersection');
-
-  entityA = {x: 10, y: 20, w: 30, h: 20};
-  entityB = {x: 20, y: 30, w: 20, h: 20};
-  actual = collision.rectangularIntersects(entityA, entityB);
-
-  t.ok(actual, 'returns truthy');
-  t.equal(actual.w, 20, 'returns width of interesction');
-  t.equal(actual.h, 10, 'returns height of intersection');
-  t.equal(actual.x, 20, 'returns x of interesction');
-  t.equal(actual.y, 30, 'returns y of intersection');
-
-  t.end();
-});
-
